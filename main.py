db6d9bdc3d082fc3d77a547b96f04168cfefe01b
Cathy Hsu <chsustudy@gmail.com>
2019-04-24 23:43:34 -0700

    Edited comments and documentation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from data_processing import Data_Processing
 ### Main execution block ###
 user_interface.banner()
 
-#Asks user for which type of data to process based on numeric input. To be passed into choose_config
+# Asks user for type of data file to process based on numeric input. To be passed into choose_config. 
 data_choice = None
 while data_choice == None:
     print('Which data file type would you like to process?\n1. Lumensphere\n2. Multimeter\n3. Serial Data\n')
@@ -20,42 +20,30 @@ output_name = user_interface.choose_output_name()
 df = Data_Processing(data_choice, config_file, input_csv, output_name)
 
 
-# Read the two sheets of the configuration file: 'Mapped' and 'General' Settings into two different DataFrames
+# Read the two sheets of the configuration file: 'Mapped' and 'General' Settings into two different dataframes
 config_df_1 = df.create_excel_dataframe(config_title, config_file.sheetnames[0])
 config_df_2 = df.create_excel_dataframe(config_title, config_file.sheetnames[1])
 
-output_columns = config_df_1['Output'].copy()
-# Create a DataFrame to hold the raw CSV file and then read said DataFrame into an Excel file 
+
+# Create a dataframe to hold the raw CSV file and then read said dataframe into an Excel file 
 raw_data_df = df.create_csv_dataframe(input_csv, config_df_2['Start Row'].loc[0])
 raw_data_excel = df.create_raw_Excelbook(raw_data_df, data_choice)
 
-
-# Convert the 'Input' and 'Output' column letters into, respectively, column titles and numbers
+# Convert the 'Input' and 'Output' column letters into, respectively, column titles and numbers. 
+# Keep a standalone copy of the 'Output.'
 col_names = raw_data_df.columns
+output_columns = config_df_1['Output'].copy()
 config_df_1 = df.convert_columns(config_df_1, col_names)
 
-
-# mapping_data_df will hold all the columns that we want to plot later
-
-# We will use col_titles_inputs as indices to extract from the raw data the columns that we want plotted
-# Note: Even though only one column is being extracted at a time, the column being extracted 
-# is stored in a DataFrame as only dataframes, not series!, can combine with other dataframes. 
-
-
-# Store the columns we want mapped into a new DataFrame 
+# Store the columns we want mapped into a new dataframe 
 mapping_data_df = df.create_mapping_dataframe(raw_data_df, config_df_1['Input'], config_df_1['Range'], config_df_1['Format'])
 
-
-# format time only if the time columns is to be mapped
-# Determine if time will serve as one of the axis of the 
-new_titles = config_df_1['Input']
-
+# Formatting time columns to be in 'Elapsed Time'
 time_unit = config_df_1['Time Unit'].dropna()
+
 # Store the new time col in a new Series temporarily, 
 # so the NaNs in mapping_data[time_title] won't convert
 # the data type into object. 
-
-# Formatting time columns to be in 'Elapsed Time'
 if (not time_unit.empty): 
     index = time_unit.index.values
     time_indices = df.letter2int(config_df_1['Input Column Numbers'])
@@ -66,11 +54,10 @@ if (not time_unit.empty):
         time_index = time_indices.loc[index[i]]
         time_title = raw_data_df.columns[time_index-1]
         
-        # Retrieve the start time and convert it to a string in elapsed time format 
-        # start_time is a Series with length 1 
+        # Retrieve the start time and convert it to a str in elapsed time format 
+        # start_time is a series of length 1 
         start_time = pd.Series(raw_data_df[time_title].loc[0])
         start_time = df.convert_to_time_object(start_time, unit)
-
 
         new_time_col = pd.DataFrame()
         new_time_col = df.convert_to_time_object(mapping_data_df[time_title], unit)
@@ -101,8 +88,6 @@ if (excel_output):
     # Create workbook to hold plotted data
     output_data_wb = df.create_plotted_workbook()
 
-
-
     # Read the output data into an Excel file
     output_data_wb = df.process_data(output_data_wb, mapping_data_df, config_df_1, output_columns)
 
@@ -118,6 +103,3 @@ if ((jpeg_output or pdf_output) and create_chart):
 # Create the PDF file 
 if (pdf_output): 
     df.make_pdf(output_name, mapping_data_df, create_chart)
-
-
-
--- user_interface.py
+++ user_interface.py
@@ -1,75 +1,40 @@
-
 from openpyxl import load_workbook
 class User_Interface:
-    def __init__(self, choice, config_file, input_csv, output_name):
-        self.choice = choice
-        self.config_file = config_file
-        self.input_csv = input_csv
-        self.output_name = output_name
+    """A class that serves as the text-based user interface"""
 
-# ENCAPSULATE INTO ANOTHER CLASS 
-#Prints program banner
 def banner():
+    """Prints a banner to indicate start of program"""
+
     print('*****************************')
     print('*  Data Processing Program  *')
     print('*****************************')
 
-
-#(1 = Lumensphere, 2 = Multimeter (not currently implemented), 3 = Serial Data (not currently implemented))
-#Asks user to enter name of configutation file, this is to avoid having to constantly change one config file (save multiple)
 def choose_config(choice):
+    """Returns a list that contains the workbook itself as well as the name of the configuration file"""
+
     if choice == 1:
         config_file = input('Enter name of Lumensphere config file: ')
     elif choice == 2:
         config_file = input('Enter name of Multimeter config file: ')
     elif choice == 3:
         config_file = input('Enter name of Serial Data config file: ')
-    #elif choice == 0: #default option for testing purposes
-     
-    #config_file = 'MultiMeterConfig'
-    config_file = 'SerialConfig'
-    #config_file = 'LumenConfig'
-    #else:
-        #choice = None
-        #print('Please enter valid input.\n')
-    
-    # return both the workbook and the name of the workbook in String format 
+
     return [load_workbook(config_file + '.xlsx'), config_file]
 
-
-#Asks user for file name of CSV to process
-#default is original Lumensphere data (testing purposes)
 def choose_csv():
+    """Asks the user for file name of the CSV"""
+
     input_csv = None
     while input_csv == None:
-        csv_choice = input('Enter name of CSV file to process or enter ''default'': ')
-        
-        if csv_choice == 'default':
-            input_csv = 'Serial_1'
-            #input_csv = 'Temp_2'
-            #input_csv = 'Lumen_1'
-        else:
-            input_csv = csv_choice
+        csv_choice = input('Enter name of CSV file to process: ')
+        input_csv = csv_choice
     return input_csv
 
-
-##asks user for file name of final excel workbook
-#default is LumenData (testing purposes)
 def choose_output_name():
+    """Asks user to input the name of the output file"""
+
     output_name = None
     while output_name == None:
-        output_choice = input('Enter name of Output file or enter ''default'': ')
-
-        if output_choice == 'default':
-            #output_name = 'MultiMeter_Output'
-            #output_name = 'LumenData'
-            output_name = 'Serial_Output'
-        else:
-            output_name = output_choice
+        output_choice = input('Enter name of Output file: ')
+        output_name = output_choice
     return output_name
-
-
-
-
-
-
